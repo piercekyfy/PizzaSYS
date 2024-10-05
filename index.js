202ffb92c2632c8840aa94291b895cd6c953b2b3
PWH <piercekfoley@gmail.com>
2024-10-05 18:26:01 +0100

    User Details form on Index page

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.set('view engine', 'pug');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/dist/bootstrap.min.js", express.static(__dirname + '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'));
 
 app.use('/', indexRouter);
 
