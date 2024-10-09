4a5f66dbaece54cd018caa3b1752b93d68575942
PWH <piercekfoley@gmail.com>
2024-10-09 23:23:29 +0100

    Login and Registration page

--- src/index.js
+++ src/index.js
@@ -14,7 +14,12 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use("/dist/bootstrap.min.js", express.static(path.join(__dirname, '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js')));
 
 app.use('/', indexRouter);
-
+app.use('/register', (req, res, next) => {
+  res.render('register');
+})
+app.use('/login', (req, res, next) => {
+  res.render('login');
+})
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 });
